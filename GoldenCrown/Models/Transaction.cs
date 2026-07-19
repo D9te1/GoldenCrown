e761fc5481359b32fc22ae89472a9eea8b22c0e4
123 <tsfrok@mail.ru>
2026-07-19 07:49:38 +0300

    Создать классы моделей

---
+++ GoldenCrown/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace GoldenCrown.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int SenderAccount {  get; set; }
+        public int ReceiverAccount {  get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
